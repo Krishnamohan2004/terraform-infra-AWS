2e5e7d5132c0b098afd1baf4da2beb5137a7bcb8
Krishnamohan2004 <krishnamohan6328@gmail.com>
2026-03-10 21:37:51 +0530

    Change availability zones for subnets

--- subnet.tf
+++ subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "subnet1" {
   vpc_id            = aws_vpc.deploy_vpc.id
   cidr_block        = "10.0.10.0/24"
-  availability_zone = "ap-south-1a"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "eks-subnet-1"
@@ -11,7 +11,7 @@ resource "aws_subnet" "subnet1" {
 resource "aws_subnet" "subnet2" {
   vpc_id            = aws_vpc.deploy_vpc.id
   cidr_block        = "10.0.20.0/24"
-  availability_zone = "ap-south-1b"
+  availability_zone = "us-east-1b"
 
   tags = {
     Name = "eks-subnet-2"
