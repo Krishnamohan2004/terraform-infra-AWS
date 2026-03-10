cabf4be2d5515793c87be4af67f6fa24cea97fe7
Krishnamohan2004 <krishnamohan6328@gmail.com>
2026-03-10 20:47:02 +0530

    Add files via upload

---
+++ subnet.tf
@@ -0,0 +1,19 @@
+resource "aws_subnet" "subnet1" {
+  vpc_id            = aws_vpc.deploy_vpc.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "eks-subnet-1"
+  }
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id            = aws_vpc.deploy_vpc.id
+  cidr_block        = "10.0.20.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "eks-subnet-2"
+  }
+}
