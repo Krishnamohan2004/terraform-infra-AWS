b3585407a79768e29b1595b675bb4115a66a7069
Krishnamohan2004 <krishnamohan6328@gmail.com>
2026-03-10 21:40:26 +0530

    Change Terraform backend region to us-east-1

--- backen.tf
+++ backen.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "rohit-terraform-state-141964413005"
     key          = "aws-devops-project/terraform.tfstate"
-    region       = "ap-south-1"
+    region       = "us-east-1"
     use_lockfile = true
   }
 }
