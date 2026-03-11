4b4eeece9807e6823e9974e2cfd87b532e3659b1
Krishnamohan2004 <krishnamohan6328@gmail.com>
2026-03-11 09:08:12 +0530

    Update S3 backend configuration for Terraform

--- backen.tf
+++ backen.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-    bucket       = "rohit-terraform-state-141964413005"
-    key          = "aws-devops-project/terraform.tfstate"
-    region       = "ap-south-1"
-    use_lockfile = true
+    bucket = "rohit-terraform-state-141964413005"
+    key    = "env/terraform.tfstate"
+    region = "ap-south-1"
   }
 }
